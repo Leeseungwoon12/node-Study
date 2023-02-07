bafdc17d7c3c85750c45e273a36a5a0e9af8d102
Leeseungwoon <seong3546@gmail.com>
2023-02-07 21:02:46 +0900

    노드 스터디 폴더 커밋

---
+++ appprac.js
@@ -0,0 +1,27 @@
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+
+// app.get('/about', (req, res) =>{
+//     res.send('about');
+// });
+
+// app.get('/user', (req,res)=>{
+//     res.send('GET user');
+// });
+
+// app.post("/posts", (req, res) => {
+//     const {user, password } = req.body;
+
+//     res.send({
+//         "user":user,
+//         "pw": password,
+
+//     });
+// });
+
+// app.listen(port, () => {
+//     console.log(`Example app listening on port ${port}`);
+// });
+
+
